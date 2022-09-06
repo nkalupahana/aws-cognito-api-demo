3121a39a6944e4ae45645e3b3d3568a07cdcb0e6
nkalupahana <avishkank@gmail.com>
2022-09-06 13:20:39 -0500

    Initial commit

---
+++ src/pages/AuthCallback.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+const AuthCallback = () => {
+    useEffect(() => {
+        (async () => {
+            const code = window.location.search.split("=")[1];
+            fetch("https://bettysbrain.auth.us-east-1.amazoncognito.com/oauth2/token", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "*/*",
+
+                  },
+                  body: `grant_type=authorization_code&code=${code}&client_id=5cpfhft5pf8877epe31kinanb8&redirect_uri=http://localhost:3000/callback`
+            }).then(resp => resp.json()).then(data => {
+                if ("access_token" in data) {
+                    localStorage.setItem("tokens", JSON.stringify(data));
+                    window.location.replace("/");
+                }
+            });
+        })();
+    }, []);
+
+    return <p>One second, we're signing you in.</p>
+};
+
+export default AuthCallback;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,84 @@
+import AWS, { CognitoIdentityCredentials, DynamoDB } from "aws-sdk";
+import { useEffect, useState } from "react";
+
+AWS.config.update({"region": "us-east-1"});
+
+const tokens = localStorage.getItem("tokens");
+let client: DynamoDB.DocumentClient | undefined = undefined;
+if (tokens) {
+    client = new DynamoDB.DocumentClient({
+        credentials: new CognitoIdentityCredentials({
+            IdentityPoolId: "us-east-1:824220c5-3885-42a2-afbc-3d9480d3d55e",
+            Logins: {
+                "cognito-idp.us-east-1.amazonaws.com/us-east-1_SOd98gjVq": JSON.parse(tokens).id_token,
+            },
+        }),
+    });
+}
+
+interface Data {
+    sessionId: string;
+    diff: any;
+}
+
+const Home = () => {
+    const [autosaveData, setAutosaveData] = useState<Data[]>([]);
+
+    useEffect(() => {
+        if (!client) return;
+        client.scan({
+            TableName: "bettysbrain_autosave"
+        }, (_, data) => {
+            if (data && data.Items) {
+                let processedData = data.Items.map(item => {
+                    return { "id": item.sessionId, "state": JSON.parse(item.state) }
+                });
+
+                let diffs = processedData.map(item => {
+                    return { "sessionId": item.id, "diff": item?.state?.actionTracker?.actions?.slice(-1)[0].diff as any }
+                }).filter(item => item.diff !== undefined);
+
+                setAutosaveData(diffs);
+            }
+        });
+    }, []);
+
+    return <div>
+        { !client && <input type="button" value="Login" onClick={() => window.location.replace("https://bettysbrain.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=5cpfhft5pf8877epe31kinanb8&redirect_uri=http://localhost:3000/callback")} /> }
+        { client && <> 
+            <p>Signed in!</p>
+            <input type="button" value="Logout" onClick={() => {
+                localStorage.removeItem("tokens");
+                window.location.replace("/");
+            }} />
+            <table>
+                <thead>
+                    <tr>
+                        <th>Session ID</th>
+                        <th>Missing Entities</th>
+                        <th>Missing Links</th>
+                        <th>Extra Links</th>
+                        <th>Reversed Links</th>
+                        <th>Backwards Links</th>
+                        <th>Shortcut Links</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { autosaveData.map(item => {
+                        return <tr key={item.sessionId}>
+                            <td>{ item.sessionId }</td>
+                            <td>{ item.diff.entities.missing }</td>
+                            <td>{ item.diff.links.missing }</td>
+                            <td>{ item.diff.links.extra }</td>
+                            <td>{ item.diff.links.reversed }</td>
+                            <td>{ item.diff.links.backwards }</td>
+                            <td>{ item.diff.links.shortcut }</td>
+                        </tr>
+                    }) }
+                </tbody>
+            </table>
+        </> }
+    </div>
+};
+
+export default Home;
